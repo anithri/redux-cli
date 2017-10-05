b864de5bf08bd9f34f777eca3736503565075645
Scott M Parrish <anithri@gmail.com>
2017-10-05 14:33:17 -0600

    Add Model - RcCollectionRaw
    This class creates an ordered list of rc files that will be read.
    It uses the BLUEPRINT_CONFIG env variable to add files to the default
    search paths.

---
+++ src/models/rc-collection-raw.js
@@ -0,0 +1,131 @@
+import _uniq from 'lodash/uniq';
+import _map from 'lodash/map';
+import _filter from 'lodash/filter';
+import findUp from 'find-up';
+import fs from 'fs';
+import path from 'path';
+
+class RcCollectionRaw {
+  constructor(opts = {}) {
+    this.fileExists = this.fileExists.bind(this);
+    this.env = opts.env || process.env;
+    this.name = opts.name || 'blueprint';
+
+    this.fs = opts.fs || fs;
+    this.rc = opts.rc || this.env.BLUEPRINT_RC || `${this.name}rc`;
+    this.cwd = opts.cwd || process.cwd();
+    this.home =
+      opts.home || this.env.HOME || this.env.HOMEPATH || this.env.USERPROFILE;
+    this.path = opts.path || path;
+    this.dotRc = opts.dotRc || `.${this.rc}`;
+    this.findUp = opts.findUp || findUp;
+    this.appData = opts.appData || this.env.APPDATA;
+    this.envFiles =
+      opts.envFiles || this.getEnvFiles(this.env.BLUEPRINT_CONFIG);
+    this.platform = opts.platform || process.platform;
+    this.userFiles = opts.userFiles || [];
+    this.systemFiles = opts.systemFiles || [];
+    this.localAppData = opts.localAppData || this.env.LOCALAPPDATA;
+    this.projectFiles = opts.projectFiles || [this.findUp(this.dotRc)];
+    this.xdgConfigDirs =
+      opts.xdgConfigDirs || (this.env.XDG_CONFIG_DIRS || '/etc/xdg').split(':');
+    this.xdgConfigHome = opts.xdgConfigHome || this.env.XDG_CONFIG_HOME;
+    this.handleOS();
+  }
+
+  rawPaths() {
+    return _uniq([
+      ...this.envFiles,
+      ...this.projectFiles,
+      ...this.userFiles,
+      ...this.systemFiles
+    ]);
+  }
+
+  paths() {
+    return _filter(this.rawPaths(), this.fileExists);
+  }
+
+  fileExists(file) {
+    return this.fs.existsSync(file) && this.fs.statSync(file).isFile();
+  }
+
+  getEnvFiles(config) {
+    if (config) {
+      return _map(config.split(':'), p => this.path.resolve(this.cwd, p));
+    } else {
+      return [];
+    }
+  }
+
+  handleOS() {
+    let userDir;
+    switch (this.platform) {
+      case 'freebsd': // pretty sure
+      /* NEEDS TESTING */
+      case 'sunos': // maybe?
+      /* NEEDS TESTING */
+      case 'linux': // for sure
+        userDir = this.path.resolve(this.home, this.xdgConfigHome || '.config');
+        this.userFiles = [
+          this.path.resolve(this.home, this.dotRc),
+          this.path.resolve(userDir, this.dotRc),
+          this.path.resolve(userDir, this.rc),
+          this.path.resolve(userDir, this.name, this.rc),
+          this.path.resolve(userDir, this.name, 'config')
+        ];
+        this.systemFiles = _map(this.xdgConfigDirs, d =>
+          this.path.resolve(d, this.rc)
+        );
+        break;
+      // https://stackoverflow.com/questions/3373948/equivalents-of-xdg-config-home-and-xdg-data-home-on-mac-os-x
+      case 'darwin':
+        /* NEEDS TESTING */
+        this.userFiles = [
+          this.path.resolve(this.home, this.dotRc),
+          this.path.resolve(
+            this.home,
+            'Library',
+            'Preferences',
+            this.name,
+            this.dotRc
+          ),
+          this.path.resolve(
+            this.home,
+            'Library',
+            'Preferences',
+            this.name,
+            this.rc
+          ),
+          this.path.resolve(
+            this.home,
+            'Library',
+            'Preferences',
+            this.name,
+            'config'
+          )
+        ];
+        this.systemFiles = [
+          this.path.resolve('/Library', 'Preferences', this.name, this.rc),
+          this.path.resolve('/Library', 'Preferences', this.name, 'config')
+        ];
+        break;
+      case 'win32':
+        /* NEEDS TESTING */
+        userDir = this.path.resolve(this.appData);
+        this.userFiles = [
+          this.path.resolve(this.home, this.dotRc),
+          this.path.resolve(this.localAppData, this.name, this.rc),
+          this.path.resolve(this.localAppData, this.name, 'config'),
+          this.path.resolve(this.appData, this.name, this.rc),
+          this.path.resolve(this.appData, this.name, 'config')
+        ];
+        this.systemFiles = []; // i can't even
+        break;
+      default:
+      // something serious unexpected happens, raise error or exit?
+    }
+  }
+}
+
+export default RcCollectionRaw;
---
+++ test/models/rc-collection-raw.test.js
@@ -0,0 +1,348 @@
+import fs from 'fs';
+import RcCollectionRaw from 'models/rc-collection-raw';
+
+const CWD = process.cwd();
+const ENV = process.env;
+delete ENV.BLUEPRINT_RC;
+
+const fakePath = {
+  resolve() {
+    return [...arguments].join('/');
+  }
+};
+const fakeFindUp = path => `findUp/${path}`;
+
+const allFakeOpts = {
+  env: 'fake',
+  fs: 'fake',
+  rc: 'fake',
+  cwd: 'fake',
+  home: 'fake',
+  path: 'fake',
+  dotRc: 'fake',
+  appData: 'fake',
+  envFiles: 'fake',
+  platform: 'fake',
+  userFiles: 'fake',
+  systemFiles: 'fake',
+  localAppData: 'fake',
+  projectFiles: 'fake',
+  xdgConfigDirs: 'fake',
+  xdgConfigHome: 'fake'
+};
+
+describe('(Models) RcCollectionRaw', () => {
+  let rawRcs, fakeRcs;
+
+  beforeEach(() => {
+    delete ENV.BLUEPRINT_RC;
+    rawRcs = new RcCollectionRaw();
+    fakeRcs = new RcCollectionRaw(allFakeOpts);
+  });
+
+  describe('Completely fakeable traits', () => {
+    test('', () => {
+      expect(fakeRcs.env).toEqual('fake');
+      expect(fakeRcs.fs).toEqual('fake');
+      expect(fakeRcs.rc).toEqual('fake');
+      expect(fakeRcs.cwd).toEqual('fake');
+      expect(fakeRcs.home).toEqual('fake');
+      expect(fakeRcs.path).toEqual('fake');
+      expect(fakeRcs.dotRc).toEqual('fake');
+      expect(fakeRcs.appData).toEqual('fake');
+      expect(fakeRcs.envFiles).toEqual('fake');
+      expect(fakeRcs.platform).toEqual('fake');
+      expect(fakeRcs.userFiles).toEqual('fake');
+      expect(fakeRcs.systemFiles).toEqual('fake');
+      expect(fakeRcs.localAppData).toEqual('fake');
+      expect(fakeRcs.projectFiles).toEqual('fake');
+      expect(fakeRcs.xdgConfigDirs).toEqual('fake');
+      expect(fakeRcs.xdgConfigHome).toEqual('fake');
+    });
+  });
+
+  describe('fs', () => {
+    test('it defaults to system fs', () => {
+      expect(rawRcs.fs).toEqual(fs);
+    });
+  });
+
+  describe('rc', () => {
+    test('it has a default value of blueprintrc', () => {
+      expect(rawRcs.rc).toEqual('blueprintrc');
+    });
+    test('it uses ENV.BLUEPRINT_RC if present', () => {
+      ENV.BLUEPRINT_RC = 'bprc';
+      const testRcs = new RcCollectionRaw();
+      expect(testRcs.rc).toEqual('bprc');
+    });
+  });
+
+  describe('cwd', () => {
+    test('it defaults to process.cwd()', () => {
+      const cwd = process.cwd();
+      expect(rawRcs.cwd).toEqual(cwd);
+    });
+  });
+
+  describe('env', () => {
+    test('it defaults to process.env', () => {
+      expect(rawRcs.env).toEqual(process.env);
+    });
+  });
+
+  describe('home', () => {
+    test('it defaults value to HOME', () => {
+      const testEnv = {
+        HOME: 'wootHOME',
+        HOMEPATH: 'wootHOMEPATH',
+        USERPROFILE: 'wootUSERPROFILE'
+      };
+      const testRcs = new RcCollectionRaw({ env: testEnv });
+      expect(testRcs.home).toEqual('wootHOME');
+    });
+    test('it defaults value to HOMEPATH', () => {
+      const testEnv = {
+        HOMEPATH: 'wootHOMEPATH',
+        USERPROFILE: 'wootUSERPROFILE'
+      };
+      const testRcs = new RcCollectionRaw({ env: testEnv });
+      expect(testRcs.home).toEqual('wootHOMEPATH');
+    });
+    test('it defaults value to USERPROFILE', () => {
+      const testEnv = {
+        USERPROFILE: 'wootUSERPROFILE'
+      };
+      const testRcs = new RcCollectionRaw({ env: testEnv });
+      expect(testRcs.home).toEqual('wootUSERPROFILE');
+    });
+  });
+
+  describe('dot_rc', () => {
+    test('', () => {
+      expect(rawRcs.dotRc).toEqual('.blueprintrc');
+    });
+  });
+
+  describe('envFiles', () => {
+    test('', () => {
+      const testOpts = {
+        env: { BLUEPRINT_CONFIG: 'Batman:Robin' },
+        cwd: 'TEST',
+        path: fakePath
+      };
+      const expectedPaths = ['TEST/Batman', 'TEST/Robin'];
+      const testRcs = new RcCollectionRaw(testOpts);
+
+      expect(testRcs.envFiles).toEqual(expectedPaths);
+    });
+  });
+
+  describe('userFiles', () => {
+    test('it returns a set of paths on linux', () => {
+      const testOpts = {
+        home: 'userHome',
+        xdgConfigHome: 'xdgConfigHome',
+        path: fakePath,
+        platform: 'linux'
+      };
+      const expectedPaths = [
+        'userHome/.blueprintrc',
+        'userHome/xdgConfigHome/.blueprintrc',
+        'userHome/xdgConfigHome/blueprintrc',
+        'userHome/xdgConfigHome/blueprint/blueprintrc',
+        'userHome/xdgConfigHome/blueprint/config'
+      ];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.userFiles).toEqual(expectedPaths);
+    });
+
+    test('it returns a set of paths on darwin', () => {
+      const testOpts = {
+        home: 'userHome',
+        path: fakePath,
+        platform: 'darwin'
+      };
+      const expectedPaths = [
+        'userHome/.blueprintrc',
+        'userHome/Library/Preferences/blueprint/.blueprintrc',
+        'userHome/Library/Preferences/blueprint/blueprintrc',
+        'userHome/Library/Preferences/blueprint/config'
+      ];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.userFiles).toEqual(expectedPaths);
+    });
+
+    test('it returns a set of paths on win32', () => {
+      const testOpts = {
+        home: 'userHome',
+        path: fakePath,
+        platform: 'win32',
+        appData: 'appData',
+        localAppData: 'localAppData'
+      };
+      const expectedPaths = [
+        'userHome/.blueprintrc',
+        'localAppData/blueprint/blueprintrc',
+        'localAppData/blueprint/config',
+        'appData/blueprint/blueprintrc',
+        'appData/blueprint/config'
+      ];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.userFiles).toEqual(expectedPaths);
+    });
+  });
+
+  describe('projectFiles', () => {
+    test('', () => {
+      const testOpts = { findUp: fakeFindUp };
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.projectFiles).toEqual(['findUp/.blueprintrc']);
+    });
+  });
+
+  describe('systemFiles', () => {
+    test('it returns a set of paths on linux', () => {
+      const testOpts = {
+        env: { XDG_CONFIG_DIRS: 'Joker:Riddler' },
+        home: 'userHome',
+        path: fakePath,
+        platform: 'linux'
+      };
+      const expectedPaths = ['Joker/blueprintrc', 'Riddler/blueprintrc'];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.systemFiles).toEqual(expectedPaths);
+    });
+
+    test('it returns a set of paths on darwin', () => {
+      const testOpts = {
+        path: fakePath,
+        platform: 'darwin'
+      };
+      const expectedPaths = [
+        '/Library/Preferences/blueprint/blueprintrc',
+        '/Library/Preferences/blueprint/config'
+      ];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.systemFiles).toEqual(expectedPaths);
+    });
+
+    test('it returns a set of paths on win32', () => {
+      const testOpts = {
+        path: fakePath,
+        platform: 'win32'
+      };
+      const expectedPaths = [];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.systemFiles).toEqual(expectedPaths);
+    });
+  });
+
+  describe('rawPaths', () => {
+    test('', () => {
+      const testOpts = {
+        env: { BLUEPRINT_CONFIG: 'Batman:Robin' },
+        cwd: 'TEST',
+        path: fakePath,
+        findUp: fakeFindUp,
+        home: 'userHome',
+        xdgConfigHome: 'xdgConfigHome',
+        path: fakePath,
+        platform: 'linux'
+      };
+      const expectedPaths = [
+        'TEST/Batman',
+        'TEST/Robin',
+        'findUp/.blueprintrc',
+        'userHome/.blueprintrc',
+        'userHome/xdgConfigHome/.blueprintrc',
+        'userHome/xdgConfigHome/blueprintrc',
+        'userHome/xdgConfigHome/blueprint/blueprintrc',
+        'userHome/xdgConfigHome/blueprint/config',
+        'Joker/blueprintrc',
+        'Riddler/blueprintrc',
+        'Joker/blueprintrc',
+        'Riddler/blueprintrc'
+      ];
+    });
+  });
+
+  describe('paths', () => {
+    test('returns rawPaths that exist', () => {
+      let flipper = 0;
+      const fakeFs = {
+        existsSync: () => true,
+        statSync: f => ({ isFile: () => !!(++flipper % 2) })
+      };
+      const testOpts = {
+        env: {
+          BLUEPRINT_CONFIG: 'Batman:Robin',
+          XDG_CONFIG_DIRS: 'Joker:Riddler'
+        },
+        cwd: 'TEST',
+        path: fakePath,
+        findUp: fakeFindUp,
+        home: 'userHome',
+        xdgConfigHome: 'xdgConfigHome',
+        path: fakePath,
+        platform: 'linux',
+        fs: fakeFs
+      };
+      const expectedPaths = [
+        'TEST/Batman',
+        // 'TEST/Robin',
+        'findUp/.blueprintrc',
+        // 'userHome/.blueprintrc',
+        'userHome/xdgConfigHome/.blueprintrc',
+        // 'userHome/xdgConfigHome/blueprintrc',
+        'userHome/xdgConfigHome/blueprint/blueprintrc',
+        // 'userHome/xdgConfigHome/blueprint/config',
+        'Joker/blueprintrc'
+        // 'Riddler/blueprintrc'
+      ];
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.paths()).toEqual(expectedPaths);
+    });
+  });
+
+  describe('fileExists', () => {
+    test('is true when file exists and is file', () => {
+      const fakeFs = {
+        existsSync: () => true,
+        statSync: () => ({ isFile: () => true })
+      };
+
+      const testOpts = { fs: fakeFs };
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.fileExists('JimGordon')).to.be.truthy;
+    });
+    test('is false when file exists and is not file', () => {
+      const fakeFs = {
+        existsSync: () => true,
+        statSync: () => ({ isFile: () => false })
+      };
+
+      const testOpts = { fs: fakeFs };
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.fileExists('JimGordon')).to.be.falsy;
+    });
+
+    test('is false when file does not exist', () => {
+      const fakeFs = {
+        existsSync: () => false,
+        statSync: () => ({ isFile: () => false })
+      };
+
+      const testOpts = { fs: fakeFs };
+      const testRcs = new RcCollectionRaw(testOpts);
+      expect(testRcs.fileExists('JimGordon')).to.be.falsy;
+    });
+  });
+
+  describe('handleOS', () => {
+    test('it exists', () => {
+      // all output is tested via .userFiles and .systemFiles
+      expect(fakeRcs.handleOS).to.exist;
+    });
+  });
+});
